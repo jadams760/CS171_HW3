4579c83120406730ffff7b711be23d37ef5db01c
Max A Stritzinger <mstritzinger@dagwood.cs.ucsb.edu>
2015-05-12 16:29:30 -0700

    adding start scripts

---
+++ start2.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+import network
+import CLI
+
+if __name__ == '__main__':
+    sites = [(1,'localhost',9991), (2,'localhost',9992), (3,'localhost',9993), (4,'localhost',9994),(5,'localhost',9995)]
+    t = CLI.CLI(2,sites,'localhost',9992,'localhost',9999)
+    t.start()
+
