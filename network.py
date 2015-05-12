8bf89350c3626558a6795e3c1995af07b16215fd
Max A Stritzinger <mstritzinger@dagwood.cs.ucsb.edu>
2015-05-12 14:50:53 -0700

    Updating network for prog_3

--- network.py
+++ network.py
@@ -11,7 +11,7 @@ class Action:
     POST = 2
 
 class Network(threading.Thread):
-    def __init__(self, port, actorLog, actorTable, actorDict, totalActors, lockDict, actorID):
+    def __init__(self, port, actorLog, actorTable, actorDict, totalActors, actorID):
         threading.Thread.__init__(self)
         self.portNum = port
         self.ipAddr = 'localhost'
@@ -22,8 +22,12 @@ class Network(threading.Thread):
         self.actorTable = actorTable
         self.transmissions = 0
         self.totalActors = totalActors
-        self.lockDict = lockDict
         self.actorID = actorID
+        self.isRequesting = False
+        self.activeLocksLock = threading.RLock()
+        self.requestedLocksLock = threading.RLock()
+        self.activeLocks = []
+        self.requestedLocks = []
 
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,48 +41,45 @@ class Network(threading.Thread):
             thread.start()
 
     def handle(self, conn):
-        firstMessage = pickle.loads(conn.recv(8192))
+        message = pickle.loads(conn.recv(4092))
         conn.send("ack")
-        newList = []
-        for i in range(firstMessage['numEvents']):
-            data = conn.recv(8192)
-            newList.append(pickle.loads(data))
+        messageType = message[0]
+        sender = message[1]
+        ## We can have two types of locks, reads and writes. We handle both here.
+        if (messageType == 'lock'):
+            lockType = message[2]
+
+            if (lockType == 'write'):
+                ## We are going to keep checking until we have no active locks to send the grant.
+                lockDict = { 'sender':sender, 'type':lockType }
+                with requestedLocksLock:
+                    requestedLocks.append(lockDict)
+                ## Loop until there are no activeLocks and we're next up, we send a grant.
+                while True:
+                    with requestedLocksLock, activeLocksLock:
+                        if not activeLocks and (requestedLocks[0] == lockDict):
+                            activeLocks.append(lockDict)
+                            break
+                conn.send("grant")
+                conn.close()
+
+            elif (lockType == 'read'):
+                lockDict = { 'sender':sender, 'type':lockType }
+                with requestedLocksLock:
+                    requestedLocks.append(lockDict)
+                ## Loop until there are no activeLocks and we're next up OR there is an activeLock, but it's a read.
+                while True:
+                    with requestedLocksLock, activeLocksLock:
+                        if (not activeLocks and (requestedLocks[0] == lockDict)) or (activeLocks and activeLocks[0]['type'] == 'read'):
+                            activeLocks.append(lockDict)
+                            break
+                conn.send("grant")
+
+        elif (messageType == 'release'):
+            with activeLocksLock:
+                activeLocks = list(filter(lambda lock: lock['sender'] != sender, activeLocks))
             conn.send("ack")
-        self.logMerge(newList)
-        self.timetableMerge(firstMessage['timeTable'], firstMessage['sourceActor'])
         conn.close()
         return
 
-
-    def logMerge(self, mergeLog):
-        # Merge the two, remove duplicates. Then update our dictionary.
-        seen = set()
-        seen_add = seen.add
-        with self.lockDict['logLock']:
-            old = set(self.actorLog)
-            for i in mergeLog:
-                if i not in self.actorLog:
-                    self.actorLog.append(i)
-        toUpdate = [x for x in mergeLog if x not in old]
-        with self.lockDict['dictLock']:
-            for i in toUpdate:
-                action = i[0]
-                if (action == Action.POST):
-                    postID = int(i[3])
-                    postMessage = i[4]
-                    self.actorDict[postID] = postMessage
-                elif(action == Action.DELETE):
-                    postID = int(i[3])
-                    self.actorDict.pop(postID, None)
-        return
-    def timetableMerge(self, newTimeTable, sourceActor):
-        # first we take the max of all elements. Then we update our own row with the max of our own row and their row.
-        with self.lockDict['timeTableLock']:
-            for i in range(self.totalActors):
-                for j in range(self.totalActors):
-                    self.actorTable[i + 1][j + 1] = max(self.actorTable[i + 1][j + 1], newTimeTable[i + 1][j + 1])
-
-            for i in range(self.totalActors):
-                self.actorTable[self.actorID][i + 1] = max(self.actorTable[self.actorID][i + 1], newTimeTable[sourceActor][i + 1])
-            return
 
