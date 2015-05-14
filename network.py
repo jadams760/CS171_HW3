d25af361a73234cfb7fb76d7c0736035236f5df6
Max A Stritzinger <mstritzinger@blondie.cs.ucsb.edu>
2015-05-13 22:11:25 -0700

    network with new DL changes

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ import sys
 from pprint import pprint
 
 class Network(threading.Thread):
-    def __init__(self, port, totalActors, actorID):
+    def __init__(self, port, totalActors, actorID, lockEvent, siteDirectory, requestID):
         threading.Thread.__init__(self)
         self.portNum = port
         self.ipAddr = 'localhost'
@@ -17,8 +17,13 @@ class Network(threading.Thread):
         self.actorID = actorID
         self.activeLocksLock = threading.RLock()
         self.requestedLocksLock = threading.RLock()
+        self.grantLock = threading.RLock()
         self.activeLocks = []
         self.requestedLocks = []
+        self.lockEvent = lockEvent
+        self.siteDirectory = siteDirectory
+        self.numGranted = 0
+        self.requestID = requestID
 
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,16 +35,19 @@ class Network(threading.Thread):
             conn, addr = sock.accept()
             thread = threading.Thread(target=self.handle,args=(conn,))
             thread.start()
+    def clearGrants(self):
+        self.numGranted = 0
 
     def handle(self, conn):
         message = pickle.loads(conn.recv(4092))
-
+        conn.close()
         messageType = message[0]
         sender = message[1]
+        senderInfo = list(filter(lambda x: x[0] == sender, self.siteDirectory))[0]
         ## We can have two types of locks, reads and writes. We handle both here.
         if (messageType == 'lock'):
             lockType = message[2]
-
+            requestID = message[3]
             if (lockType == 'write'):
                 ## We are going to keep checking until we have no active locks to send the grant.
                 lockDict = { 'sender':sender, 'type':lockType }
@@ -52,8 +60,11 @@ class Network(threading.Thread):
                             self.activeLocks.append(lockDict)
                             self.requestedLocks.remove(lockDict)
                             break
-                conn.send(pickle.dumps("grant"))
-                conn.close()
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket.connect((senderInfo[1],senderInfo[2]))
+                sock.send(pickle.dumps(['grant', self.actorID, requestID]))
+                sock.close()
+
 
             elif (lockType == 'read'):
                 lockDict = { 'sender':sender, 'type':lockType }
@@ -66,15 +77,29 @@ class Network(threading.Thread):
                             self.activeLocks.append(lockDict)
                             self.requestedLocks.remove(lockDict)
                             break
-                conn.send(pickle.dumps("grant"))
-
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket.connect((senderInfo[1],senderInfo[2]))
+                sock.send(pickle.dumps(['grant', self.actorID, requestID]))
+                sock.close()
         elif (messageType == 'release'):
             with self.activeLocksLock:
                 self.activeLocks = list(filter(lambda lock: lock['sender'] != sender, self.activeLocks))
+        elif (messageType == 'grant'):
+            requestID = message[2]
+            ## Ignore any requests that are not our current request ID.
+            if (requestID != self.requestID['ID']):
+                return
+            with self.grantLock:
+                self.numGranted += 1
+                if (self.numGranted >= 3):
+                    self.lockEvent.set()
+        elif (messageType == 'nevermind'):
+            with self.requestedLocksLock, self.activeLocksLock:
+                self.requestedLocksLock = list(filter(lambda lock: lock['sender'] != sender, self.activeLocks))
+                self.activeLocks = list(filter(lambda lock: lock['sender'] != sender, self.activeLocks))
 
-            
-            conn.send(pickle.dumps("ack"))
-        conn.close()
+
+
         return
 
 
